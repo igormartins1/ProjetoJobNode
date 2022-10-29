f43b688f6145a60da5117fbd5a38f77fce284247
IgorMartins01 <igor.almeida@rbmweb.com.br>
2022-10-29 10:56:28 -0300

    Criando Models

---
+++ models/Job.js
@@ -0,0 +1,42 @@
+const sequelize= require('sequelize');
+const db = require('../db/conection');
+
+// criando um metodo sequelize  um model
+
+const Job = db.define('job',{
+
+    title: {
+        type: sequelize.STRING,
+    },
+
+    description: {
+        type: sequelize.STRING,
+    },
+
+    salary: {
+        type: sequelize,STRING,
+    },
+    
+    company:{
+        type: sequelize.STRING,
+    },
+
+    email:{
+        type: sequelize.STRING,
+
+    },
+    new_job: {
+        type: sequelize.INTEGER,
+    }
+
+
+});
+
+// Exportando o Modulo
+
+module.exports=Job;
+
+
+
+
+
